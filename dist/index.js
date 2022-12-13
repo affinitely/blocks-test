7ae57802da212e56d6e7770b84f8b4c6b8fde469
DESKTOP-BF3VM9G\John Mandy Samonte <johnmandysamonte@gmail.com>
2022-12-13 16:25:59 +0800

    babel fixing

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
 "use strict";
 
-require("core-js/modules/es.object.define-property.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
 var _button = _interopRequireDefault(require("./components/button"));
 require("./index.css");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var _default = {
-  Button: _button["default"]
+  Button: _button.default
 };
-exports["default"] = _default;
+exports.default = _default;
