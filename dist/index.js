032a8ffae33750cb483ba69473227b197d746e1e
DESKTOP-BF3VM9G\John Mandy Samonte <johnmandysamonte@gmail.com>
2022-12-13 17:04:34 +0800

    fix export

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _button.default;
+  }
+});
 var _react = _interopRequireDefault(require("react"));
 var _button = _interopRequireDefault(require("./components/button"));
 require("./index.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-var _default = {
-  Button: _button.default
-};
-exports.default = _default;
