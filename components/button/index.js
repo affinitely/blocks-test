3c4aa7310fa74f92b3df91109f2b5c6c528f8f20
DESKTOP-BF3VM9G\John Mandy Samonte <johnmandysamonte@gmail.com>
2022-12-13 15:37:47 +0800

    modified button

--- components/button/index.js
+++ components/button/index.js
@@ -1,39 +1,9 @@
-import Block from "../Block";
-import isLightColored from "../../utils";
-import classNames from "classnames";
+import React from "react";
 
-export const ACTION_CLOSE = 'close';
-export const ACTION_SUBMIT = 'submit';
-export const ACTION_LINK = 'link';
-
-export default function Button({
-                                   action,
-                                   text,
-                                   fontSize,
-                                   fontWeight,
-                                   textColor,
-                                   backgroundColor,
-                                   blockStyle
-                               }) {
-    const derivedColor = textColor || (
-        backgroundColor === 'transparent' ? "#1e293b" :
-            (isLightColored(backgroundColor || '') ? "#1e293b" : "white")
-    )
-    return (
-        <Block {...blockStyle}>
-            <button
-                className={classNames("x-aff-p-button", {
-                    "transparent": !textColor && backgroundColor === 'transparent'
-                })}
-                style={{
-                    fontSize,
-                    fontWeight,
-                    color: backgroundColor && backgroundColor !== 'transparent' && derivedColor,
-                    backgroundColor
-                }}
-            >
-                {text}
-            </button>
-        </Block>
-    )
+export default function Button({ children }) {
+  return (
+    <button className="px-3 py-2 rounded-lg bg-black text-white">
+      {children || "This is button from affinitely-package-test"}
+    </button>
+  );
 }
